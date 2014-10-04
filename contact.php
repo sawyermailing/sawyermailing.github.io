edbb19a49314afb88830f980673e8a1788b5a200
sawyermailing <ryan@sawyermailingsystems.com>
2014-10-04 13:17:29 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,16 +1,12 @@
-<html>
-<body>
+
 <?php
+$name = $_POST['name'];
+$request = $_POST['request'];
+
 $to = "clearchaos3@gmail.com";
-$subject = "Online Parts Request";
-$email = $_REQUEST['message'];
-$headers = "From: $email";
-$sent = mail($to, $subject, $message, $headers);
-if($sent)
-{print ('<a href="index.html">Thank you. Click here to return to the Homepage.</a>');}
-else
-{print ("We have encountered an error sending your message";}
+$subject = "Online Supplies Request";
+$body = "This is an automated message, please don't reply.\n\n $request"
+
+mail($to,$subject,$body);
 	
 ?>
-</body>
-</html>
